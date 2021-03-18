d6fa60b7fa4a58ae577e2381d1eb4f755e7ebe73
Lais Filoso de Freitas <laisfiloso@ibm.com>
2021-03-17 21:19:17 -0300

    aula 2 e 3

---
+++ projeto_01/projetoEventos/nodeJS/nodeEventos/app.js
@@ -0,0 +1,38 @@
+
+var express = require('express');
+
+var load = require('express-load');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
+
+
+var load = require('express-load');
+
+var error = require('./middlewares/error');
+
+var app = express();
+
+// view engine setup
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(cookieParser('nodeEventos'));
+app.use(expressSession());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + '/public'));
+
+load('models')
+ .then('controllers')
+ .then('routes')
+ .into(app);
+
+//middlewares
+app.use(error.notFound);
+app.use(error.serverError);
+
+app.listen(3000, function() {
+  console.log('Aplicação no ar')
+});
---
+++ projeto_01/projetoEventos/nodeJS/nodeEventos/controllers/eventos.js
@@ -0,0 +1,33 @@
+module.exports = function (app) {
+    var EventosController = {
+        menu: function (request, response) {
+            var usuario = request.session.usuario,
+            params = { usuario: usuario };
+            response.render('eventos/menu', params);
+        },
+
+        cadastroUsuario: function (request, response) {
+            var usuario = request.session.usuario,
+            params = { usuario: usuario };
+            response.render('eventos/cadUsuario', params);
+        },
+        cadastroEvento: function (request, response) {
+            var usuario = request.session.usuario,
+            params = { usuario: usuario };
+            response.render('eventos/cadEvento', params);
+        },
+        listaEventos: function (request, response) {
+            var usuario = request.session.usuario,
+            params = { usuario: usuario };
+            response.render('eventos/listaEventos', params);
+        },
+        novoEvento: function (request, response) {
+            response.redirect('/menu');
+        }
+
+    };
+
+
+
+    return EventosController;
+};
