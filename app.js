1773e7c3fa54c22181f2d844c35572511e2b3283
Lais Filoso de Freitas <laisfiloso@ibm.com>
2021-03-18 23:05:19 -0300

    add mongo

--- app.js
+++ app.js
@@ -13,6 +13,19 @@ var error = require('./middlewares/error');
 
 var app = express();
 
+var mongoose = require('mongoose');
+global.db = mongoose.connect('mongodb://localhost:27017/neventos', { useNewUrlParser: true });
+
+mongoose.connection.on('connected', function () {
+ console.log('=====Conexão estabelecida com sucesso=====');
+});
+mongoose.connection.on('error', function (err) {
+ console.log('=====Ocorreu um erro: ' + err);
+});
+mongoose.connection.on('disconnected', function () {
+ console.log('=====Conexão finalizada=====');
+});
+
 // view engine setup
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -33,9 +46,9 @@ load('models')
 app.use(error.notFound);
 app.use(error.serverError);
 
-var porta = process.env.PORT || 8080;
-app.listen(porta);
+// var porta = process.env.PORT || 8080;
+// app.listen(porta);
 
-// app.listen(3000, function() {
-//   console.log('Aplicação no ar')
-// });
+app.listen(3000, function() {
+  console.log('Aplicação no ar')
+});
--- controllers/home.js
+++ controllers/home.js
@@ -1,4 +1,7 @@
 module.exports = function (app) {
+    var mongoose = require('mongoose');
+    var Usuario = mongoose.model('usuarios');
+
     var HomeController = {
         index: function (req, res) {
             res.render('home/index');
@@ -6,15 +9,19 @@ module.exports = function (app) {
         login: function (request, response) {
             var nome = request.body.usuario.nome;
             var senha = request.body.usuario.senha;
+            var query = { 'nome': nome, 'senha': senha };
 
-            if (nome == 'admin' && senha == 'admin') {
-                var usuario = request.body.usuario;
+            Usuario.findOne(query).select('nome senha')
+            .exec(function (erro, usuario) {
+
+                if (erro) {
+                response.redirect('/');
+                }
+                else {
                 request.session.usuario = usuario;
                 response.redirect('/menu');
-            }
-            else {
-                response.redirect('/');
-            }
+                }
+            });
         },
 
         logout: function (request, response) {
@@ -26,8 +33,23 @@ module.exports = function (app) {
             var nome = request.body.usuario.nome;
             var senha = request.body.usuario.senha;
             var confirma = request.body.usuario.confirma;
-        //código a ser implementado
-            response.redirect('/menu');
+
+            if ((senha != confirma) || nome.trim().length == 0) {
+                response.redirect('/');
+            }
+            else {
+                var usuario = request.body.usuario;
+
+                Usuario.create(usuario, function (erro, usuario) {
+
+                    if (erro) {
+                        response.redirect('/');
+                    }
+                    else {
+                        response.redirect('/menu');
+                    }
+                });
+            }
         }
     };
 
