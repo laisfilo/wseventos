1a44b22b06d458a2920c3dd0ee922c2b67e175ce
Lais Filoso <laisfiloso@gmail.com>
2021-03-23 10:16:59 -0300

    angular js

---
+++ ProjetoEventos/Mean-AngularJs/appAngularJs/controllers/eventos.js
@@ -0,0 +1,10 @@
+module.exports = function (app) {
+    var EventosController = {
+
+        index: function (req, res) {
+            res.render('eventos/index');
+        }
+        
+    };
+    return EventosController;
+};
